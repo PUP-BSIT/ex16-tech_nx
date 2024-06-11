acfd8f196acf5c89ac08a28b214cf85c538c6405
Sofia <sabarrantes2911@gmail.com>
2024-06-11 14:35:18 +0800

    change id name into snake case in html and js

--- barrantes/index.js
+++ barrantes/index.js
@@ -1,68 +1,73 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let comments = [];
+let comments = [];
 
-  function addComment(event) {
-    event.preventDefault();
-    console.log("Add comment");
-    let name = document.querySelector("#fname").value;
-    let commentText = document.querySelector("#comment").value;
-    let date = new Date().toLocaleDateString();
+function addComment(event) {
+  event.preventDefault();
+  console.log("Add comment");
+  let name = document.querySelector("#fname").value;
+  let commentText = document.querySelector("#comment").value;
+  let date = new Date().toLocaleDateString();
 
-    let comment = { name, comment: commentText, date };
+  let comment = { name, comment: commentText, date };
 
-    comments.push(comment);
-    sortComments();
-    displayComments();
+  comments.push(comment);
+  commentManager.sortComments();
+  commentManager.displayComments();
 
-    document.querySelector("#fname").value = "";
-    document.querySelector("#comment").value = "";
-    document.querySelector("#com-b").disabled = true;
-  }
+  document.querySelector("#fname").value = "";
+  document.querySelector("#comment").value = "";
+  document.querySelector("#com_b").disabled = true;
+}
 
-  function displayComments() {
+const commentManager = {
+  displayComments: function() {
     let commentList = document.querySelector("#comments_section");
     commentList.innerHTML = "";
 
-    comments.forEach((comment) => {
+    comments.forEach(function(comment) {
       let li = document.createElement("li");
       li.textContent = `${comment.comment} - ${comment.name} 
           - ${comment.date}`;
       commentList.appendChild(li);
     });
-  }
+  },
 
-  function sortComments(order = "asc") {
+  sortComments: function(order = "asc") {
     if (order === "asc") {
-      comments.sort((a, b) => a.name.localeCompare(b.name));
+      comments.sort(function(a, b) {
+        return a.name.localeCompare(b.name);
+      });
     } else if (order === "desc") {
-      comments.sort((a, b) => b.name.localeCompare(a.name));
+      comments.sort(function(a, b) {
+        return b.name.localeCompare(a.name);
+      });
     }
   }
+};
 
-  function comment_btn() {
-    let name = document.getElementById("fname").value;
-    let comment = document.getElementById("comment").value;
-    let button = document.getElementById("com-b");
+function comment_btn() {
+  let name = document.getElementById("fname").value;
+  let comment = document.getElementById("comment").value;
+  let button = document.getElementById("com_b");
 
-    button.disabled = !(name.length && comment.length);
-  }
+  button.disabled = !(name.length && comment.length);
+}
 
-  document
-    .querySelector("#comments_form")
-    .addEventListener("submit", addComment);
-  document.querySelector("#fname").addEventListener("input", comment_btn);
-  document.querySelector("#comment").addEventListener("input", comment_btn);
-  document.querySelector("#asc_btn").addEventListener("click", function () {
-    sortComments("asc");
-    displayComments();
-  });
-  document.querySelector("#desc_btn").addEventListener("click", function () {
-    sortComments("desc");
-    displayComments();
-  });
-
-  displayComments();
+document
+  .querySelector("#comments_form")
+  .addEventListener("submit", addComment);
+document.querySelector("#fname").addEventListener("input", comment_btn);
+document.querySelector("#comment").addEventListener("input", comment_btn);
+document.querySelector("#asc_btn").addEventListener("click", function() {
+  commentManager.sortComments("asc");
+  commentManager.displayComments();
 });
+document.querySelector("#desc_btn").addEventListener("click", function() {
+  commentManager.sortComments("desc");
+  commentManager.displayComments();
+});
+
+commentManager.displayComments();
+
 
 function searchCountry() {
     let countryName = document.getElementById("countryInput").value.trim();
